9ee47b7a9ae261cb236d2f3c7dbde843cf925388
Lucas Covre Delboni <lucas_cd460@hotmail.com>
2017-04-28 13:01:45 -0300

    Ainda não funciona e ta todo cagado
    erros de ponteiros frequentes e prints perdidos

--- main.c
+++ main.c
@@ -18,7 +18,6 @@ char* nroUSP2() {
     return("0000000");
 }
 
-
 // elemento das listas de adjacência e de resposta
 typedef struct estr {
         int v; // elemento
@@ -34,6 +33,15 @@ typedef struct
        NO* inicio;
 } VERTICE;
 
+typedef struct aux {
+        int v;
+        struct aux *prox;
+}REGISTRO, *PONT;
+
+typedef struct {
+  PONT inicio;
+  PONT fim;
+} FILA;
 
 // funcao principal
 //NO *caminho(int N, int *ijpeso, int *aberto, int inicio, int fim, int chave);
@@ -44,7 +52,40 @@ typedef struct
 // e outras funcoes auxiliares que esta
 // necessitar
 //------------------------------------------
-colocaAbertos(VERTICE *g, int *abertos, int A){
+int removeElementoFila(FILA *f){
+    //printf("tentando remover");
+    int i= f->inicio->v;
+    //printf("vai remover o %d ", i);
+    PONT apagar = f->inicio;
+    f->inicio=f->inicio->prox;
+    free(apagar);
+    if(f->inicio==NULL){
+        f->fim=NULL;
+    }
+    return i;
+}
+
+FILA *inicializaFila(){
+	FILA *f = (FILA*)malloc(sizeof(FILA));
+	f->inicio=NULL;
+	f->fim=NULL;
+	return f;
+}
+
+void adicionaElementoFila(FILA *f, int var){
+    REGISTRO *novo = (REGISTRO*)malloc(sizeof(REGISTRO));
+    novo->prox=NULL;
+    novo->v=var;
+    if(f->inicio==NULL){
+        f->inicio=novo;
+    }
+    else{
+        f->fim->prox=novo;
+    }
+    f->fim=novo;
+}
+
+void colocaAbertos(VERTICE *g, int *abertos, int A){
     for(int i=1;i<=A;i++){
         if(abertos[i-1]==true){
             g[i].aberto = true;
@@ -55,32 +96,91 @@ colocaAbertos(VERTICE *g, int *abertos, int A){
     }
 }
 
-void insereVertices(int *arestas, VERTICE *g, int A, int *abertos){
-	for(int i=1;i<=A*3;i=i+3){
-		NO* novo = malloc(sizeof(NO));
-		novo->v = arestas[i];
-		novo->peso=arestas[i+1];
-		novo->prox=NULL;
-		if(g[i-1].inicio==NULL){
-			g[i-1].inicio=novo;
-		}
-		else{
-			NO *p = g[i-1].inicio;
-			g[i-1].inicio = novo;
-			g[i-1].inicio->prox=p;
-		}
-	}
-	colocaAbertos(g, abertos, A);
+void insereVertice(VERTICE *g, int i, int j, int p){
+    NO * novo = (NO*) malloc(sizeof(NO));
+    if(g[i].inicio){
+        printf("inicio:%d no elmento %d", g[i].inicio, i);
+    }
+	novo->v=j;
+	novo->peso=p;
+	novo->prox=NULL;
+	g[i].inicio=novo;
+//	printf("teste:%d->%d\n",i,g[i].inicio->v);
 }
 
-buscaProfundidade(VERTICE *g, int inicio, int fim){
-    g[inicio].flag=1;
-    NO * p = g[inicio].inicio;
+void insereVertices(int *arestas, VERTICE *g, int A,int N, int *abertos){
+    //inicializa
+    for(int i=1;i<=N;i++){
+        g[i].inicio=NULL;
+    }
+	for(int i=0;i<A;i++){
+
+        int k=i*3;
+	    printf("%d - %d - %d \n",arestas[k],arestas[k+1],arestas[k+2]);
+        insereVertice(g,arestas[k],arestas[k+1],arestas[k+2]);
+    }
+    //colocaAbertos(g, abertos, A);
+
+
+    //imprime grafo:
+	for(int i=1;i<=N;i++){
+		NO *p = g[i].inicio;
+		printf("\n%d ->", i);
+		while(p){
+			printf(" %d", p->v);
+			p=p->prox;
+			printf("p=%d",p);
+		}
+	}
+	printf("inseriu certo");
+
+
+}
+
+void buscaProfundidade(VERTICE *g, int i, int fim){
+    printf("busca produndidade \n");
+    g[i].flag=1;
+    NO * p = g[i].inicio;
+    printf("teste %d %d",i,p->v);
     while (p){
         if (g[p->v].flag==0){
+                printf("%d -> %d",i,p->v);
             buscaProfundidade(g, p->v, fim);
         }
         p=p->prox;
+    }
+}
+
+void zeraFlags(VERTICE *g, int N){
+    for(int i=1;i<=N;i++){
+        g[i].flag=0;
+    }
+}
+
+
+
+
+void buscaLargura(VERTICE *g, int inicio, int fim, int N){
+    zeraFlags(g,N);
+    FILA *f = inicializaFila(inicio);
+    adicionaElementoFila(f, inicio);
+    printf("\n");
+    g[inicio].flag=1;
+    while(f){
+        int i = removeElementoFila(f);
+        NO *p = g[i].inicio;
+        //printf("dddd");
+        while(p){
+            if(g[p->v].flag==0){
+                //printf("fffff");
+                //printf("%d ->", i);
+                adicionaElementoFila(f, p->v);
+                g[p->v].flag=1;
+                printf("colocou flag 1");
+            }
+            p=p->prox;
+        }
+        g[i].flag=2;
     }
 }
 
@@ -91,7 +191,10 @@ NO *caminho(int N, int A, int *ijpeso, int *aberto, int inicio, int fim, int cha
 
 	// seu codigo AQUI
 	VERTICE *g = malloc(sizeof(VERTICE)*(N));
-	insereVertices(ijpeso,g,A, aberto);
+	insereVertices(ijpeso,g,A,N, aberto);
+	buscaProfundidade(g,1,3);
+
+	buscaLargura(g,1,3,N);
 	//...
 
 	return resp;
@@ -106,7 +209,6 @@ int main() {
 
 
 	// aqui vc pode incluir codigo de teste
-
 	// exemplo de teste trivial
 
 	int N=3; // grafo de 3 vértices 1..3
@@ -115,14 +217,13 @@ int main() {
 	int fim=3;
 	int chave=2;
 	int A = 3;
-	int ijpeso[]={1,2,10, 2,3,20, 3,1,10};
+	int ijpeso[]={1,2,10, 2,3,20, 3,1,10, 2,1,5};
 
 	// o EP sera testado com uma serie de chamadas como esta
 	NO* teste = NULL;
 	teste = caminho(N,A, ijpeso, aberto, inicio, fim, chave);
 	printf("ok");
 	return teste;
-
 }
 
 // por favor nao inclua nenhum código abaixo da função main()
